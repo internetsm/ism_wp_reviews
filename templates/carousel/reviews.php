201a268e729b699bbafb7f7e74a42e02aa027009
Michele Capicchioni <capimichi@gmail.com>
2017-11-07 11:59:51 +0100

    Commit

--- templates/carousel/reviews.php
+++ templates/carousel/reviews.php
@@ -9,12 +9,12 @@
 ?>
 
 <ul class="slick-carousel" data-columns="<?php echo $carousel['columns']; ?>" data-scroll-columns="<?php echo $carousel['scroll_columns']; ?>" data-autoplay="<?php echo $carousel['autoplay']; ?>" data-arrows="<?php echo $carousel['arrows']; ?>" data-dots="<?php echo $carousel['dots']; ?>">
-<?php foreach ($offers as $offer){
+<?php foreach ($reviews as $review){
     ?>
-    <li class="offer columns-<?php echo $carousel['columns']; ?>">
-        <img class="offer-image" src="<?php echo $offer['image']; ?>"/>
-        <h2 class="offer-title"><?php echo $offer['title']; ?></h2>
-        <div class="offer-content"><?php echo substr($offer['description'], 0, 100); ?></div>
+    <li class="review columns-<?php echo $carousel['columns']; ?>">
+        <img class="review-image" src="<?php echo $review['image']; ?>"/>
+        <h2 class="review-title"><?php echo $review['title']; ?></h2>
+        <div class="review-content"><?php echo substr($review['description'], 0, 100); ?></div>
         <button></button>
     </li>
     <?php
